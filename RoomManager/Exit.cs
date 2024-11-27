97149edcdbdd4837513e81a8d9a421bda2e33170
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-11-27 22:52:30 +0900

    1127 22:52 RoomManager & Dungeon1 Setting Added

--- RoomManager/Exit.cs
+++ RoomManager/Exit.cs
@@ -31,10 +31,17 @@ public class Exit : MonoBehaviour
         
     }
 
+    //Exitオブジェクトが何かと接触したら
     void OnTriggerEnter2D(Collider2D collision)
     {
+        //接触した相手がPlayerだったら
         if(collision.gameObject.tag == "Player")
         {
+            //RoomManagerクラスのChangeSceneメソッドの力を使い、
+            //Exit自身がもっている「目的のシーン」と「ドア番号」の情報を引数に指定しながらシーンを切り替える。
+            //これにより、目的のシーンに切り替わる際に、同時に該当のExitのドア番号がstatic変数のDoorNumberに記録され、
+            //次シーンの冒頭(Start)において、
+            //RoomManagerがstatic変数に記憶されたドア番号と同じ番号を所持するExitオブジェクトを検索していく行為に繋がる
             RoomManager.ChangeScene(sceneName,doorNumber);
         }
     }
