78073b0dce64bcbea2f689350b58952b8416af06
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-11-29 11:09:57 +0900

    1129 11:09 Door Added

---
+++ Item/Door.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public int arrangeId = 0; // セーブする際の識別番号
+    public bool isGoldDoor = false; //自分が金のドアかどうか
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //ぶつかった相手がPlayer
+        if (collision.gameObject.tag == "Player")
+        {
+            //自分がゴールドなら
+            if (isGoldDoor)
+            {
+                //ゴールドキーを持っていれば
+                if (ItemKeeper.hasGoldKeys > 0)
+                {
+                    //ゴールドキーを消耗
+                    ItemKeeper.hasGoldKeys--;
+                    Destroy(gameObject); //自分を削除
+                }
+            }
+            else //自分がゴールドではない
+            {
+                //シルバーキーを持っていれば
+                if (ItemKeeper.hasSilverKeys > 0)
+                {
+                    //シルバーキーを消耗
+                    ItemKeeper.hasSilverKeys--;
+                    Destroy(gameObject); //自分を削除
+                }
+            }
+        }
+    }
+}
--- Player/ItemKeeper.cs
+++ Player/ItemKeeper.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class ItemKeeper : MonoBehaviour
 {
 
-    public static int hasGoldKeys = 0; //金のカギ
-    public static int hasSilverKeys = 0; //銀のカギ
+    public static int hasGoldKeys = 5; //金のカギ
+    public static int hasSilverKeys = 10; //銀のカギ
     public static int hasArrows = 100; //矢の残数
-    public static int hasLights = 0; //ライトの残数
+    public static int hasLights = 10; //ライトの残数
 
     // Start is called before the first frame update
     void Start()
